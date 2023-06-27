dee6065118fd4219d24a0bb5d5cab6622cc83848
webcrawls <git@kaden.sh>
2023-06-27 19:19:43 -0400

    ♻️(paper) simplify

--- paper/src/main/java/live/webcrawls/midas/paper/MidasPlugin.java
+++ paper/src/main/java/live/webcrawls/midas/paper/MidasPlugin.java
@@ -1,25 +1,40 @@
 package live.webcrawls.midas.paper;
 
-import live.webcrawls.midas.api.formatter.ChatFormatter;
-import live.webcrawls.midas.common.module.GreentextChatFormatter;
+import io.papermc.paper.chat.ChatRenderer;
+import io.papermc.paper.event.player.AsyncChatEvent;
+import live.webcrawls.midas.api.context.ChatContext;
+import live.webcrawls.midas.api.sender.ChatSender;
+import live.webcrawls.midas.common.MidasPlatform;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
-public class MidasPlugin extends JavaPlugin {
+public class MidasPlugin extends JavaPlugin implements Listener, ChatRenderer {
 
-    private PaperMidasPlatform chatService;
-    private List<ChatFormatter> formatters;
+    private MidasPlatform platform;
 
     @Override
     public void onEnable() {
-        this.getLogger().info("hello, world");
-        this.formatters = new ArrayList<>();
-        this.formatters.add(new GreentextChatFormatter());
-
-        this.chatService = new PaperMidasPlatform(this, this.formatters);
-        this.chatService.registerListeners();
+        this.platform = new MidasPlatform(this.getDataFolder());
+        this.platform.loadConfiguration();
+        this.getServer().getPluginManager().registerEvents(this, this);
     }
 
+    @EventHandler
+    public void onAsyncChat(AsyncChatEvent event) {
+        event.renderer(this);
+    }
+
+    @Override
+    public @NotNull Component render(@NotNull Player source, @NotNull Component sourceDisplayName, @NotNull Component message, @NotNull Audience viewer) {
+        ChatSender sender = ChatSender.immutable(source.getUniqueId(), source.getName(), Map.of());
+        return this.platform.formatContext(ChatContext.immutable(sender, message, PlainTextComponentSerializer.plainText().serialize(message))).message();
+    }
 }
